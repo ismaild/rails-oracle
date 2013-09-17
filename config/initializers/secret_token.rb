ce1e3d81cd9fe03227f4c42dad7314d396aeec39
Ismail Dhorat <ismail@zyelabs.net>
2013-09-17 10:12:29 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appex::Application.config.secret_token = 'f88e5d4f7fd48d50573261b73659097cb1f2350f241704ab8c4fdafd8777731e2e31d28b7db9c79f187c5ffae0a27179ffb885c1d8f253b86f6b9dc7a00b7959'
